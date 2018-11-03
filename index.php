d787119cd373477a615289dc51f5360ccb63b8c0
NoahFlowa <osterhoutn@gmail.com>
2018-11-03 15:56:08 -0400

    Added the fucking files

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- 
+		Who the fuck combines their source code?  Programmers that have servers that only have 1GB of Storage?  Wrong motherfucker
+		I got storage for fucking days.  Here read my code, nice and clean!  You can just read it like a book, how it fucking
+		should be!
+
+		But  honestly motherfucker, check out the Github repo if you want.  Feel free to fork this and create your own
+		just give me a shout out you fuck :D
+
+		Repo: 
+    -->
+
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>The Best Fucking Website</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<!-- FontAwesome 5 CDN -->
+	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+	<!-- Google Fonts API | Open Sans -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="main.css">
+
+</head>
+
+<body>
+	<div class="jumbotron jumbotron-fluid" style="text-align: center;">
+		<div class="container">
+			<h1 class="display-4">The Best Fucking Website</h1>
+			<br>
+			<p class="lead">This is still a <a href="http://bettermotherfuckingwebsite.com">motherfucking website</a>, but it's using Bootstrap 4 motherfucker.</p>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<!-- Nothing to see here motherfucker -->
+			</div>
+			<div class="col-10">
+				<h3>You think you're website is soo fucking cool because it's simple?</h3>
+				<p>Well welcome to 2018, where bare bones websites are fucking lame. Put some fucking styling in this shit, it doesn't take that long.</p>
+
+				<br>
+
+				<h3>3 fucking technologies to make a website better</h3>
+				<ul>
+					<li>Bootstrap 4</li>
+					<li>Google Fonts</li>
+					<li>Fontawesome 5</li>
+				</ul>
+			</div>
+			<div class="col">
+				<!-- Or here -->
+			</div>
+		</div>
+	</div>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
